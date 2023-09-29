cc2b657e3db55e2f0e66ee9f3701ff96ce912b1c
TrokNet <54963472+TrokNet@users.noreply.github.com>
2023-09-29 08:58:48 -0500

    Update formulas.js

--- formulas.js
+++ formulas.js
@@ -1,16 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Crear arreglos para almacenar los números
-    var a = [];
-    var b = [];
-    var c = [];
     var todo = [];
 
     // Función para recalcular los números al azar
     function recalcularNumerosAzar() {
-        // Limpiar arreglos
-        a = [];
-        b = [];
-        c = [];
         todo = [];
 
         // Generar 3 números aleatorios
@@ -22,37 +14,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 numero = Math.floor(Math.random() * 100) + 1; // Generar número aleatorio entre 1 y 100
 
                 // Comprobar si el número es único en el arreglo "todo"
-                if (!todo.includes(numero) && !todo.includes(numero - 1) && !todo.includes(numero - 2) && !todo.includes(numero + 1) && !todo.includes(numero + 2)) {
+                if (
+                    !todo.includes(numero) &&
+                    !todo.includes(numero - 1) &&
+                    !todo.includes(numero - 2) &&
+                    !todo.includes(numero + 1) &&
+                    !todo.includes(numero + 2)
+                ) {
                     valor = true;
                 }
             }
 
             // Comprobar si es múltiplo de 10
             if (numero % 10 === 0) {
-                a.push(numero);
-                a.push(numero - 1);
-                a.push(numero - 2);
-
                 todo.push(numero);
                 todo.push(numero - 1);
                 todo.push(numero - 2);
             }
             // Comprobar si es múltiplo de 11
             else if ((numero - 1) % 10 === 0) {
-                b.push(numero);
-                b.push(numero + 1);
-                b.push(numero + 2);
-
                 todo.push(numero);
                 todo.push(numero + 1);
                 todo.push(numero + 2);
             }
             // Si no es múltiplo de 10 ni de 11
             else {
-                c.push(numero);
-                c.push(numero + 1);
-                c.push(numero - 1);
-
                 todo.push(numero);
                 todo.push(numero + 1);
                 todo.push(numero - 1);
@@ -69,12 +55,8 @@ document.addEventListener("DOMContentLoaded", function () {
         // Mostrar los arreglos en la lista HTML "todo"
         mostrarEnLista(todo, "todo");
 
-        // Muestra el número al azar en el párrafo
-        var numeroAzarParrafo = document.getElementById("numeroAzar");
-        numeroAzarParrafo.textContent = "Números al azar: " + numerosAlAzar.join(", ");
-
         // Llamar a la función para crear la matriz 10x10 y colorear los valores
-        crearMatriz(10, 10, a, b, c);
+        crearMatriz(10, 10, todo);
     }
 
     // Asociar el evento clic al botón
@@ -89,14 +71,14 @@ document.addEventListener("DOMContentLoaded", function () {
         var lista = document.getElementById(idLista);
         lista.innerHTML = ''; // Limpiar la lista antes de agregar elementos
         arreglo.forEach(function (elemento) {
-            var listItem = document.createElement("li");
+            var listItem = document.createElement("d");
             listItem.textContent = elemento;
             lista.appendChild(listItem);
         });
     }
 
     // Función para crear y mostrar la matriz
-    function crearMatriz(filas, columnas, arregloA, arregloB, arregloC) {
+    function crearMatriz(filas, columnas, todo) {
         var tabla = document.querySelector('table');
         tabla.innerHTML = ''; // Limpiar la tabla antes de generar una nueva matriz
 
@@ -108,14 +90,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 celda.textContent = valor.toString();
 
                 // Agregar clases para colorear según los arreglos
-                if (arregloA.includes(valor)) {
-                    celda.classList.add('todo');
-                } else if (arregloB.includes(valor)) {
-                    celda.classList.add('todo');
-                } else if (arregloC.includes(valor)) {
+                if (todo.includes(valor)) {
                     celda.classList.add('todo');
                 }
-
                 fila.appendChild(celda);
             }
             tabla.appendChild(fila);
